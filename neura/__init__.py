82a1f1a4cbf93ed119ac22a4ee1292de178c1de8
Sanjarbek <ismatovsanjarbek@yandex.ru>
2025-02-23 13:17:17 +0500

    Big update

--- neura/__init__.py
+++ neura/__init__.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, session
 from uuid import uuid4
 from .auth import login_required
+from .chat import get_my_history
 
 
 def create_app():
@@ -11,18 +12,20 @@ def create_app():
         SECRET_KEY=f'{uuid4()}',
         DATABASE=os.path.join(app.instance_path, 'database.sqlite'),
     )
+    
 
 
     @app.route('/')
     @login_required
     def home():
+        g.random_room_id = str(f'{uuid4()}')
+        return render_template('home.html', history=get_my_history)
 
-        return render_template('home.html')
-
-    from . import db, auth
+    from . import db, auth,chat
     db.init_app(app)
 
     app.register_blueprint(auth.auth)
+    app.register_blueprint(chat.chat)
     try:
         os.makedirs(app.instance_path)
     except OSError:
---
+++ neura/chat.py
@@ -0,0 +1,45 @@
+from flask import (
+    Blueprint, flash, g, get_flashed_messages, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug import exceptions
+
+from uuid import uuid4
+from neura.db import get_db
+
+chat = Blueprint('chat', __name__, url_prefix='/chat')
+
+@chat.route('/new') # it is like creating room
+def create_chat():
+    db = get_db()
+    id = f'{uuid4()}'
+    db.execute("INSERT INTO chat (id, summary_title, owner) VALUES (?, ?, ?)", (id, "Empty chat", session.get('user_id')))
+    db.commit()
+    
+    return redirect(url_for('chat.chat_view', chat_id=id))
+
+@chat.route('/<chat_id>',methods=('GET', 'POST'))
+def chat_view(chat_id):
+    db = get_db()
+
+    check_url = db.execute("SELECT EXISTS(SELECT 1 FROM chat WHERE id = ?)", (chat_id, )).fetchone()[0]
+    
+    if not check_url:
+        raise OSError()
+    
+    if not db.execute('SELECT * FROM chat WHERE id = ?', (chat_id, )).fetchone()['owner'] == session.get('user_id'):
+        return exceptions.Forbidden()
+    
+
+    if request.method == 'POST':
+        user_query = request.form['query']
+        
+
+        db.execute('INSERT INTO query (msg, owner, chat) VALUES (?, ?, ?)', (user_query, session.get('user_id'), chat_id))
+        db.commit()
+    
+    return render_template('chat/chat_home.html', chat_id=chat_id)
+
+def get_my_history():
+    db = get_db()
+    return db.execute("SELECT * FROM chat WHERE owner = ?", (session.get('user_id'), ))
